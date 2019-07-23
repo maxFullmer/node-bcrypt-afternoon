e2f6c02df3a9b25d64f9944892968d22f999816e
Max-F <max.fullmer@outlook.com>
2019-07-22 20:24:02 -0700

    July 22, 2019

--- server/authMiddleware.js
+++ server/authMiddleware.js
@@ -4,5 +4,12 @@ module.exports = {
             res.status(401).send('Please log in')
         }
         next();
+    },
+
+    adminsOnly: (req, res, next) => {
+        if(!req.session.user.isAdmin) {
+            res.status(403).send('You are not an Admin');
+        }
+        next();
     }
 }
--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -19,27 +19,38 @@ export default class Container extends Component {
   }
 
   getDragonTreasure() {
-    // axios GET to /api/treasure/dragon here
     axios.get('/api/treasure/dragon')
     .then(response => {
       this.setState({
-        treasures: {...this.state.treasures, dragon: response.data}
+        treasures: { ...this.state.treasures, dragon: response.data }
       })
     }).catch(error => console.log(error));
   }
 
   getAllTreasure() {
-    // axios GET to /api/treasure/all here
+    axios.get('/api/treasure/all')
+    .then( treasure => {
+      this.setState({
+        treasures: {
+          ...this.state.treasures,
+          all: treasure.data
+        }
+      })
+    }).catch( error => alert(error.response.request.response))
   }
 
   getMyTreasure() {
-    // axios GET to /api/treasure/user here
-    axios.get('/api/treasure/user')
-    .then(response => {
-      this.setState({
-        treasures: { ...this.state.treasures, user: response.data}
+    axios
+      .get('/api/treasure/user')
+      .then(treasure => {
+        this.setState({
+          treasures: {
+            ...this.state.treasures,
+            user: treasure.data,
+          },
+        });
       })
-    }).catch(error => alert(error.response.request.response));
+      .catch(error => alert(error.response.request.response));
   }
 
   addMyTreasure(newMyTreasure) {
